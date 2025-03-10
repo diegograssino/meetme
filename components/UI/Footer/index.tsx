c5a17d7573d854ff73a7a36503c10dcdee6b6dd8
diegograssino <diegograssino@gmail.com>
2025-03-10 11:48:34 -0300

    Refactor css system, add footer content, add tailwind file to get to work vscode extension

--- components/UI/Footer/index.tsx
+++ components/UI/Footer/index.tsx
@@ -1,11 +1,39 @@
+import {
+  GitHubLogoIcon,
+  HeartFilledIcon,
+  LinkedInLogoIcon,
+} from "@radix-ui/react-icons";
+import Link from "next/link";
 import Container from "../Container";
 
 const Footer = () => {
   return (
-    <footer className="mt-auto">
-      <div className="h-4 pt-4 border-t border-accent" />
-      <Container className="pt-8 pb-4">Footer</Container>
-    </footer>
+    <div className="mt-auto">
+      <div className="h-4" />
+      <footer className="text-xs border-t border-accent ">
+        <Container className="py-4 flex justify-between items-center gap-1">
+          <div className="flex items-center gap-3">
+            <p className="flex flex-row justify-center gap-1">
+              <span>Made with</span>
+              <span className="flex items-center">
+                <HeartFilledIcon width={12} />
+              </span>
+              <span>by</span>
+              <span className="font-bold">turbodisk</span>
+            </p>
+          </div>
+          <div className="flex items-center gap-3">
+            <Link href="">
+              <GitHubLogoIcon width={12} />
+            </Link>
+            <span>|</span>
+            <Link href="">
+              <LinkedInLogoIcon width={12} />
+            </Link>
+          </div>
+        </Container>
+      </footer>
+    </div>
   );
 };
 
