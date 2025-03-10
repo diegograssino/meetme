976a48ad3811872ba05025544c34400290126a7c
diegograssino <diegograssino@gmail.com>
2025-03-10 17:08:54 -0300

    Navbar content workaround

--- components/UI/Navbar/index.tsx
+++ components/UI/Navbar/index.tsx
@@ -1,9 +1,15 @@
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import Container from "../Container";
 
 const Navbar = () => {
   return (
     <nav className="py-2 mb-4 sticky z-30 border-b border-accent">
-      <Container>Navbar</Container>
+      <Container className="flex justify-between items-center">
+        <h6 className="py-1 px-2 text-accent hover:cursor-pointer">MeetMe</h6>
+        <button className="animation-on-hover py-1 px-2 rounded cursor-pointer ">
+          <HamburgerMenuIcon />
+        </button>
+      </Container>
     </nav>
   );
 };
