c9edeb450333cdc4fb370e5835a5ce77612ec3c1
Diego Grassino <diegograssino@gmail.com>
2025-03-19 22:23:16 -0300

    Add createdAt prop

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,6 +1,6 @@
-import CalendarPanel from '@/components/shared/CalendarPanel'
-import UserPanel from '@/components/shared/UserPanel'
-import Container from '@/components/UI/Container'
+import CalendarPanel from "@/components/shared/CalendarPanel";
+import UserPanel from "@/components/shared/UserPanel";
+import Container from "@/components/UI/Container";
 
 const CreatePage = () => {
   return (
@@ -8,10 +8,10 @@ const CreatePage = () => {
       className="flex w-full flex-col gap-sections lg:h-[calc(100vh-var(--height-navbar)-(var(--gap-sections)*2))] lg:flex-row"
       as="main"
     >
-      <UserPanel user="" />
+      <UserPanel user="" createdAt="" />
       <CalendarPanel availability={[]} />
     </Container>
-  )
-}
+  );
+};
 
-export default CreatePage
+export default CreatePage;
